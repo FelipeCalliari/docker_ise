bb11d7556cabc5bfa7666c96d5ef8ac7253bc17d
FelipeCalliari <harrycalliari@gmail.com>
2024-11-26 15:23:30 -0300

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,16 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mkdir -p config
+#DISPLAY=$(ifconfig br0 | awk '/inet / {print $6}'):0
+#echo "$DISPLAY"
 
-# DISPLAY=`ipconfig getifaddr en0`:0
+# Set MAC Address
+LICENSE_MAC=${LICENSE_MAC:-"01:ab:23:cd:45:ef"}
 
-docker run -it \
-           -v /tmp/.X11-unix:/tmp/.X11-unix \
-           -v "$HOME/.Xauthority":/home/xilinx/.Xauthority:ro \
-           -e DISPLAY="$DISPLAY" \
-           -v "$HOME":/home/xilinx/shared \
-           -v "$PWD/config":/home/xilinx/.config/Xilinx \
-           -v /etc/localtime:/etc/localtime:ro \
-           -e QT_X11_NO_MITSHM=1 \
-           --net=host --ipc=host \
-           xilinx_ise
+# Set XAUTHORITY, if not set
+XAUTHORITY="${XAUTHORITY:-${HOME}/.Xauthority}"
+#echo "$XAUTHORITY"
+
+docker run -it --rm \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v "$XAUTHORITY":/home/xilinx/.Xauthority:ro \
+    -v "$HOME":/home/xilinx/shared \
+    -v "$HOME"/.config/Xilinx:/home/xilinx/.config/Xilinx \
+    -v /dev:/dev --device-cgroup-rule='c *:* rmw' \
+    -e QT_X11_NO_MITSHM=1 \
+    -e DISPLAY="$DISPLAY" \
+    --net=host --ipc=host \
+    --mac-address "$LICENSE_MAC" \
+    --name docker-ise \
+    xilinx_ise $@
+
+#bash
